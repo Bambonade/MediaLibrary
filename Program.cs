3a669d75af305e32b54a6123286b034f441ec08e
Brandon Nelson <machinecar998@gmail.com>
2022-09-29 08:40:51 -0500

    MovieFile

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace MediaLibrary
 
             string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
             logger.Info(scrubbedFile);
+            MovieFile movieFile = new MovieFile(scrubbedFile);
 
             logger.Info("Program ended");
         }
