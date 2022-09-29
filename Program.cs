347dd309cda111939b5334fd4a94bbabbe77f010
Brandon Nelson <machinecar998@gmail.com>
2022-09-29 08:26:57 -0500

    Album class

--- Program.cs
+++ Program.cs
@@ -18,6 +18,17 @@ namespace MediaLibrary
                 genres = { "Comedy", "Romance" }
             };
             Console.WriteLine(movie.Display());
+
+            Album album = new Album
+            {
+                mediaId = 321,
+                title = "Greatest Album Ever, The (2020)",
+                artist = "Jeff's Awesome Band",
+                recordLabel = "Universal Music Group",
+                genres = { "Rock" }
+            };
+            Console.WriteLine(album.Display());
+            
             logger.Info("Program ended");
         }
     }
